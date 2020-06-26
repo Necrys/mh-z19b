d4589f9ad7ba3b963a6a71223ac2626e2f78e0a1
Alexander.Kulinich <necrysgd@gmail.com>
2020-06-26 19:30:06 +0300

    add autocalibration config option

--- mh-z19b.go
+++ mh-z19b.go
@@ -10,12 +10,14 @@ type Config struct {
   Address          string
   MeasurementRange uint32
   Debug            bool
+  Autocalibration  bool
 }
 
 type Sensor interface {
   SetMeasurementRange( max uint32 ) error
   GetMeasurement()( uint32, error )
   Close()
+  SetAutocalibration( bool ) error
 }
 
 type sensor struct {
@@ -48,6 +50,7 @@ func NewSensor( c *Config )( Sensor, error ) {
   s.port = port
 
   s.SetMeasurementRange( s.cfg.MeasurementRange )
+  s.SetAutocalibration( s.cfg.Autocalibration )
 
   if s.cfg.Debug {
     log.Println( "Sensor successfully created" )
@@ -109,6 +112,8 @@ func ( s *sensor )SetMeasurementRange( max uint32 ) error {
     return errors.New( "Bad response" )
   }
 
+  s.cfg.MeasurementRange = max
+
   return nil
 }
 
@@ -166,6 +171,67 @@ func ( s *sensor )GetMeasurement()( uint32, error ) {
   measure := ( uint32( resp[ 2 ] ) << 8 ) + uint32( resp[ 3 ] )
 
   return measure, nil
+}
+
+func ( s *sensor )SetAutocalibration( enable bool ) error {
+  // 0xFF, 0x01, 0x79, 0xA0, 0x00, 0x00, 0x00, 0x00, 0x00
+  var cmd = []byte { 0xff, 0x01, 0x79, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+  if enable {
+    cmd[ 3 ] = 0xA0
+  } else {
+    cmd[ 3 ] = 0x00
+  }
+
+  for i := 1; i < 8; i++ {
+    cmd[ 8 ] = cmd[ 8 ] + cmd[ i ]
+  }
+  cmd[ 8 ] = 255 - cmd[ 8 ]
+  cmd[ 8 ] = cmd[ 8 ] + 1
+
+  if s.cfg.Debug {
+    log.Println( "SetAutocalibration, cmd{ ",
+      cmd[ 0 ], cmd[ 1 ], cmd[ 2 ], cmd[ 3 ], cmd[ 4 ], cmd[ 5 ], cmd[ 6 ], cmd[ 7 ], cmd[ 8 ], " }" )
+  }
+
+	if _, err := s.port.Write( cmd ); err != nil {
+		return err
+	}
+
+  var read = 0
+  var resp = []byte { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+  
+  for read < 9 {
+    n, err := s.port.Read( resp[ read: ] )
+    if err != nil {
+      return err
+    }
+
+    if s.cfg.Debug {
+      log.Println( "SetAutocalibration, read ", n, " bytes" )
+    }
+    
+    read += n
+  }
+
+  if s.cfg.Debug {
+    log.Println( "SetAutocalibration, resp{ ",
+      resp[ 0 ], resp[ 1 ], resp[ 2 ], resp[ 3 ], resp[ 4 ], resp[ 5 ], resp[ 6 ], resp[ 7 ], resp[ 8 ], " }" )
+  }
+
+  var crc = byte( 0x00 )
+  for i := 1; i < 8; i++ {
+    crc += resp[ i ]
+  }
+  crc = 255 - crc
+  crc += 1
+
+  if resp[ 0 ] != 0xff || resp[ 1 ] != 0x79 || resp[ 8 ] != crc {
+    return errors.New( "Bad response" )
+  }
+  
+  s.cfg.Autocalibration = enable
+
+  return nil
 }
 
 func ( s *sensor )Close() {
